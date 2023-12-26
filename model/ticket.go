2fdbc07dd4e6d5b58992559698cd0f2d702365d0
HeroBuxx <me@herobuxx.me>
2023-12-26 17:10:30 +0700

    server: Update ticket data struct
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- model/ticket.go
+++ model/ticket.go
@@ -10,11 +10,12 @@ type Ticket struct {
 	ID          string `json:"id"`
 	ArtistID    string `json:"artistid" gorm:"column:artistid"`
 	Title       string `json:"title"`
-	Price       string `json:"price"`
+	Price       int    `json:"price"`
 	Location    string `json:"location"`
 	Date        string `json:"date"`
 	Description string `json:"description"`
-	Sold        string `json:"sold"`
+	Sold        int    `json:"sold"`
+	Likes       int    `json:"sold"`
 }
 
 // Users struct
